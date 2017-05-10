1f1652fb122e2b6e663e6f3d96961632d49fbf3f
JohnAntonioCollins <john@johncollins.ws>
2017-05-10 01:11:00 -0400

    mock pi4j test

---
+++ mic-bot/src/test/java/collins/john/model/MoveMotorsTest.java
@@ -0,0 +1,41 @@
+/**
+ * John Antonio Collins 4/27/17
+ **/
+//TODO: change bracket style to "Alman"
+package collins.john.model;
+
+import com.pi4j.io.gpio.GpioController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MoveMotorsTest
+{
+
+    //TODO: Q: what to mock exactly for R'Pi? GpioController or individual pins or something in between?
+    //TODO: fix 64 bit library error
+
+    @Mock
+    GpioController gpioController;
+
+    @InjectMocks
+    private MoveMotors moveMotors;
+
+    public MoveMotorsTest() throws Exception
+    {
+    }
+
+    @Test
+    public void toggleLED_test()
+    {
+        moveMotors.toggleLED();
+        verify(gpioController).toggle();
+        verifyNoMoreInteractions(gpioController);
+    }
+}
